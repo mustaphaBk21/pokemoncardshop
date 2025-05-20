2ddca4d8703044c9b893b38c61ebe1d74984f88a
Mustapha Ben khelifa <benkhelifamustapha21@gmail.com>
2025-05-20 13:38:54 +0100

    server

--- Backend/index.js
+++ Backend/index.js
@@ -4,14 +4,14 @@ const db = require('./config/db')
 const cors = require('cors')
 const port = 5000
 const pokmeonroutes= require('./routes/pokemon')
-const userroutes  = require('./routes/user')
+
 
 
 // 
 app.use(cors())
 app.use(express.json())
 app.use('/pokemons' , pokmeonroutes)
-app.use('/users',userroutes)
+
 
 // 
 app.get('/',(req,res)=>{
